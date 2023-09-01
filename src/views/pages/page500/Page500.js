e0d380da726fe25b7ed9c3d41b3bf6a1414c328e
xDev2022-power <22comzace@gmail.com>
2023-09-02 04:12:36 +0800

    Create Page500.js

---
+++ src/views/pages/page500/Page500.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import {
+  CButton,
+  CCol,
+  CContainer,
+  //CFormInput,
+  CInputGroup,
+  //CInputGroupText,
+  CRow,
+} from '@coreui/react'
+//import CIcon from '@coreui/icons-react'
+//import { cilMagnifyingGlass } from '@coreui/icons'
+
+import {useNavigate} from 'react-router-dom';
+
+const Page500 = () => {
+
+  const navigate = useNavigate();
+
+  function handleClick() {
+    navigate('/Dashboard')
+  }
+  return (
+    <div className="bg-light min-vh-100 d-flex flex-row align-items-center">
+      <CContainer>
+        <CRow className="justify-content-center">
+          <CCol md={6}>
+            <span className="clearfix">
+              <h1 className="float-start display-3 me-4">500</h1>
+              <h4 className="pt-3">Houston, we have a problem!</h4>
+              <p className="text-medium-emphasis float-start">
+                The page you are looking for is temporarily unavailable.
+              </p>
+            </span>
+            <CInputGroup className="input-prepend">
+
+             
+              <CButton color="info" onClick={handleClick}>Back to Main Page</CButton>
+            </CInputGroup>
+          </CCol>
+        </CRow>
+      </CContainer>
+    </div>
+  )
+}
+
+export default Page500
